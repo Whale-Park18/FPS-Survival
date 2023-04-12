18de5f869c489dafa6dd920c1f800dcbe8b5ea93
Whale-Park18 <96812003+Whale-Park18@users.noreply.github.com>
2023-04-12 16:50:18 +0900

    Update EnemyObjectPool.cs * T 타입 변경     * old: EnemyFSM     * new: EnemyBase * 멤버 변수 추가     * -target: Transform * 프로퍼티 추가     +Target: Transform * 메소드     * +GetObject(Vector3): EnemyBase         * isTrackActiveObject 활성화 되었을 때, 추적 자료형에 추가하는 처리 추가     * +ReturnObject(EnemyBase): void         * 오버라이딩         * isTrackActiveObject 활성화 되었을 때, 추적 자료형에서 제거하는 처리 추가 * 주석 추가 및 변경

--- Assets/Code/ObjectPool/EnemyObjectPool.cs
+++ Assets/Code/ObjectPool/EnemyObjectPool.cs
@@ -1,13 +1,31 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 using WhalePark18.Character.Enemy;
+using WhalePark18.Manager;
 
 namespace WhalePark18.ObjectPool
 {
-    public class EnemyObjectPool : MonoObjectPool<EnemyFSM>
+    /// <summary>
+    /// Enemy 오브젝트풀
+    /// </summary>
+    /// <remarks>
+    /// 오브젝트 추적
+    /// </remarks>
+    public class EnemyObjectPool : MonoObjectPool<EnemyBase>
     {
+        private Transform target;
+
+        public Transform Target { get; set; }
+
+        protected override EnemyBase CreateObject()
+        {
+            GameObject instance = GameObject.Instantiate(prefab);
+            EnemyBase enemy = instance.GetComponent<EnemyBase>();
+            enemy.Setup(idToAssignToTheObject++, target);
+
+            return enemy;
+        }
+
         /// <summary>
         /// 오브젝트 반환 인터페이스
         /// </summary>
@@ -15,7 +33,7 @@ namespace WhalePark18.ObjectPool
         /// <remarks>
         /// Vector3(0, 0, 0) 위치 오브젝트
         /// </remarks>
-        public override EnemyFSM GetObject()
+        public override EnemyBase GetObject()
         {
             return GetObject(Vector3.zero);
         }
@@ -28,15 +46,35 @@ namespace WhalePark18.ObjectPool
         /// <remarks>
         /// position 위치 오브젝트
         /// </remarks>
-        public EnemyFSM GetObject(Vector3 position)
+        public EnemyBase GetObject(Vector3 position)
         {
+            /// 1. 오브젝트풀이 비었는 경우 오브젝트 생성
             if (objectPool.Count == 0)
                 CreateObjects();
 
-            EnemyFSM enemyFSM = objectPool.Dequeue();
-            EnableObject(enemyFSM, position);
+            /// 2. 오브젝트풀에서 오브젝트를 받고 활성화 시킨다.
+            EnemyBase enemy = objectPool.Dequeue();
+            EnableObject(enemy, position);
 
-            return enemyFSM;
+            /// 3. trackActiveObject가 활성화 되었을 때, 오브젝트를 활성화 오브젝트 자료형에 추가한다.
+            if (isTrackActiveObject)
+            {
+                activeObjects.Add(enemy.ID, enemy);
+            }
+
+            return enemy;
+        }
+
+        public override void ReturnObject(EnemyBase returnObject)
+        {
+            /// 반환된 오브젝트를 비활성화 한 후, 오브젝트풀에 넣는다.
+            base.ReturnObject(returnObject);
+
+            /// trackActiveObject가 활성화 되었을 때, 활성화 오브젝트 추적 자료형에서 제거한다.
+            if(isTrackActiveObject)
+            {
+                activeObjects.Remove(returnObject.ID);
+            }
         }
     }
 }
