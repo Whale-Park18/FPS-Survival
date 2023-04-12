b0d948adeeff60c4f91e72b9f1ce2b544085fd6c
Whale-Park18 <96812003+Whale-Park18@users.noreply.github.com>
2023-04-12 22:41:55 +0900

    Update EnemyManager.cs * +enum EnemyClass 정의 * +enum KillCountInfo 정의 * -struct EnemySpawnInfo 정의 * 멤버 변수     * delete -enemyObjectPool: EnemyObjectPool     * add -enemyObjectPools: EnemyObjectPool[]     * delete -enemySpawnTileObjectPool: EnemySpawnTileObjectPool     * add -enemySpawnTileObjectPools: EnemySpawnTileObjectPool[]     * add -target: Transform     * delete -currentEnemyCount: int     * delete -maxEnemyCount: int     * delete -numberOfEnemiesSpawnedAtOnce: int     * delete -enemySpawnCycleTime: float     * delete -enemySpawnDelayTime: float     * add -enemySpawnInfos: EnemySpawnInfo     * rename -enemyHeight: float         * old: enemyHeight         * new: overlapHeight     * rename -enemyRadius: float         * old: enemyRadius         * new: overlapRadius * 프로퍼티     * delete +Target: Transform     * delete +KillCount: int     * add +KillCountInfo: KillCountInfo * 메소드     * 초기화(Initialize) 관련 메소드 수정         * delete -OnSInitialized(void): void         * -Awake(void): void             * OnSInitialized 메소드에서 하던 초기화 역할 전담     * add +Setup(Transform): void     * add +Reset(void): void     * delete -Run(void): void     * add +SpawnEnemyBackground(EnemyClass): IEnumerator     * add +SpawnEnemyEvent(EnemyClass): void     * -SpawnTile(void): void 매개변수 변경         * old: SpawnTile(void)         * new: SpawnTile(EnemyClass)     * -SpawnEnemy(EnemySpawnPoint): IEnumerator 매개변수 변경         * old: SpawnEnemy(EnemySpawnPoint)         * new: SpawnEnemy(EnemySpawnPoint, EnemyClass)     * +ReturnEnemy(EnemyFSM): void         * 매개변수 변경             * old: ReturnEnemy(EnemyFSM)             * new: ReturnEnemy(EnemyBase)         * Enemy의 등급에 맞게 오브젝트풀에 반환하고 처치 수를 갱신한다.         * Normal 등급 처치 수가 10의 배수일 때, Elite 등급 소환 코드 작성 * 주석 추가 및 변경

--- Assets/Code/Manager/EnemyManager.cs
+++ Assets/Code/Manager/EnemyManager.cs
@@ -7,27 +7,45 @@ using WhalePark18.ObjectPool;
 
 namespace WhalePark18.Manager
 {
+    public enum EnemyClass { Normal = 0, Elite, Boss }
+
+    public struct KillCountInfo
+    {
+        public int normal;
+        public int elite;
+        public int boss;
+    }
+
     public class EnemyManager : MonoSingleton<EnemyManager>
     {
-        private EnemyObjectPool enemyObjectPool;
-        private EnemySpawnTileObjectPool enemySpawnTileObjectPool;
+        [System.Serializable]
+        private struct EnemySpawnInfo
+        {
+            public int      maxSpawnCount;          // 최대 소환 수
+            public int      currentSpawnCount;      // 현재 소환 수
+            public int      numberOfSpawnAtOnce;    // 한 번에 소환되는 수
+            public float    spawnCycleTime;         // 적 소환 주기
+            public float    spawnDelayTime;         // 타일 생성 후, 소혼되기까지 대기 시간
+        }
+
+        /****************************************
+         * EnemyManager Base
+         ****************************************/
+        [SerializeField]
+        private EnemyObjectPool[]           enemyObjectPools;
+        [SerializeField]
+        private EnemySpawnTileObjectPool[]  enemySpawnTileObjectPools;
+        private Transform                   target;                     // Enemy 공격 목표
 
         [Header("Enemy Spawn")]
-        private int currentEnemyCount;  // 현재 소환된 적의 수
-        [SerializeField, Tooltip("소환된 적의 최대 수")]
-        private int maxEnemyCount;
-        [SerializeField, Tooltip("한 번에 소환되는 적의 수")]
-        private int numberOfEnemiesSpawnedAtOnce;
-        [SerializeField, Tooltip("적 소환 주기")]
-        private float enemySpawnCycleTime;
-        [SerializeField, Tooltip("타일 생성 후, 적이 소환하기까지 대기 시간")]
-        private float enemySpawnDelayTime;
+        [SerializeField, Tooltip("Normal, Elite, Boss 순")]
+        private EnemySpawnInfo[] enemySpawnInfos;
 
         [Header("Overlap Position")]
-        [SerializeField, Tooltip("enemy 캡슐 콜라이더의 높이")]
-        private float enemyHeight;
-        [SerializeField, Tooltip("enemy 캡슐 콜라이더의 반지름")]
-        private float enemyRadius;
+        [SerializeField, Tooltip("중복 확인에 사용될 높이")]
+        private float overlapHeight;
+        [SerializeField, Tooltip("중복 확인에 사용될 반지름")]
+        private float overlapRadius;
         [SerializeField, Tooltip("Pillar 게임오브젝트의 레이어")]
         private LayerMask pillarLayerMask;
         [SerializeField, Tooltip("Pillar과 겹쳤을 때, 회피할 반지름")]
@@ -37,60 +55,97 @@ namespace WhalePark18.Manager
         [SerializeField, Tooltip("맵 크기")]
         private Vector2 mapSize;
 
-        private Transform target;   // Enemy 공격 목표
-        private int killCount;      // Enemy 처치수
+        /****************************************
+         * Kill Info
+         ****************************************/
+        private KillCountInfo killCountInfo;
 
-        public Transform Target
-        {
-            set => target = value;
-            get => target;
-        }
+        /****************************************
+         * 프로퍼티
+         ****************************************/
+        public KillCountInfo KillCountInfo => killCountInfo;
 
-        public int KillCount => killCount;
-
+        /// <summary>
+        /// EnemyManager가 인스턴스화 된 직후, 호출되는 초기호 메소드
+        /// </summary>
         private void Awake()
         {
+            /// 1. 싱글톤 작업
             if (Instance != null && Instance != this)
             {
                 Destroy(this.gameObject);
             }
 
-            OnSInitialized();
+            /// 2. 컴포넌트 및 변수 초기화
+            //enemySpawnInfos = new EnemySpawnInfo[3];
+            killCountInfo = new KillCountInfo();
         }
 
-        private void OnSInitialized()
+        public void Setup(Transform target)
         {
-            enemyObjectPool = GetComponentInChildren<EnemyObjectPool>();
-            enemySpawnTileObjectPool = GetComponentInChildren<EnemySpawnTileObjectPool>();
+            this.target = target;
+
+            for(int i = 0; i < enemyObjectPools.Length; i++)
+            {
+                enemyObjectPools[i].Target = target;
+            }
         }
 
-        public void Run()
+        public void Reset()
         {
-            print("Enemy Manager Run");
-            StartCoroutine(SpawnTile());
+            /// 1. 소환된 모든 적 오브젝트풀에 반환
+            foreach (var objectPool in enemyObjectPools)
+                objectPool.Reset();
+
+            /// 2. 발사된 모든 투사체 오브젝트 풀에 반환
+            /// (지속 시간을 생각해 볼 때, 안해도될 것 같음)
+        }
+
+        /// <summary>
+        /// 지속적으로 Enemy를 소환해야 할 때, 사용하는 메소드
+        /// </summary>
+        /// <param name="spawnEnemyClass">소환할 적의 등급</param>
+        /// <returns>코루틴</returns>
+        public IEnumerator SpawnEnemyBackground(EnemyClass spawnEnemyClass)
+        {
+            WaitForSeconds waitSpawnEnemyCycleTime = new WaitForSeconds(enemySpawnInfos[(int)spawnEnemyClass].spawnCycleTime);
+
+            while (true)
+            {
+                /// spawnEnemyClass의 소환 정보를 통해 조건을 확인한다.
+                /// 1. spawnCount < numberOfSpawnAtOnce
+                /// 2. currentSpawnCount < maxSpawnCount
+                /// (단, 두 조건 모두 만족할 때 적을 소환한다.)
+                for (int spawnCount = 0; spawnCount < enemySpawnInfos[(int)spawnEnemyClass].numberOfSpawnAtOnce && enemySpawnInfos[(int)spawnEnemyClass].currentSpawnCount < enemySpawnInfos[(int)spawnEnemyClass].maxSpawnCount; spawnCount++)
+                {
+                    if (spawnEnemyClass == EnemyClass.Normal)
+                        enemySpawnInfos[(int)EnemyClass.Normal].currentSpawnCount++;
+
+                    SpawnTile(spawnEnemyClass);
+                }
+
+                yield return waitSpawnEnemyCycleTime;
+            }
+        }
+
+        /// <summary>
+        /// 트리거에 의해 Enemy이 소환되야 할 때, 사용하는 메소드
+        /// </summary>
+        /// <param name="spawnEnemyClass">소환할 적의 등급</param>
+        public void SpawnEnemyEvent(EnemyClass spawnEnemyClass)
+        {
+            SpawnTile(spawnEnemyClass);
         }
 
         /// <summary>
         /// 적 소환시 미리 알려주는 타일(EnemySpawnTile)을 소환하는 메소드
         /// </summary>
+        /// <param name="spawnEnemyClass">소환할 적의 등급</param>
         /// <returns>코루틴</returns>
-        private IEnumerator SpawnTile()
+        private void SpawnTile(EnemyClass spawnEnemyClass)
         {
-            while(true)
-            {
-                /// 동시에 numberOfEnemiesSpawnedAtOnce 숫자만큼 적을 소환한다.
-                /// 단, maxEnemyCount를 넘을 수는 없다.
-                for (int i = 0; i < numberOfEnemiesSpawnedAtOnce && currentEnemyCount < maxEnemyCount; i++)
-                {
-                    currentEnemyCount++;
-                    EnemySpawnPoint spawnPoint = enemySpawnTileObjectPool.GetObject(GetRandomSpawnPoint());
-                    StartCoroutine(SpawnEnemy(spawnPoint));
-                }
-
-                /// 적 소환 주기 시간까지 대기
-                yield return new WaitForSeconds(enemySpawnCycleTime);
-            }
-
+            EnemySpawnPoint spawnPoint = enemySpawnTileObjectPools[(int)spawnEnemyClass].GetObject(GetRandomSpawnPoint());
+            StartCoroutine(SpawnEnemy(spawnPoint, spawnEnemyClass));
         }
 
         /// <summary>
@@ -112,9 +167,6 @@ namespace WhalePark18.Manager
             if (IsOverlapOnPillar(randomSpawnPosition))
                 randomSpawnPosition = CalculateAvoidOverlapPosition(randomSpawnPosition);
 
-            WhalePark18.Debug.Log(DebugCategory.Debug, "Spawn Tile Position",
-                "Enemy{0} SpawnPosition: {1}", currentEnemyCount, randomSpawnPosition);
-
             return randomSpawnPosition;
         }
 
@@ -128,8 +180,8 @@ namespace WhalePark18.Manager
             /// interactionObjectMask 레이어마스크에 겹치는 오브젝트 개수가 1이상이면
             /// 기둥에 겹치는 것을 의미한다.
             Vector3 point0 = position;
-            Vector3 point1 = position + Vector3.up * enemyHeight;
-            int overlapCount = Physics.OverlapCapsuleNonAlloc(point0, point1, enemyRadius, null, pillarLayerMask);
+            Vector3 point1 = position + Vector3.up * overlapHeight;
+            int overlapCount = Physics.OverlapCapsuleNonAlloc(point0, point1, overlapRadius, null, pillarLayerMask);
 
             return overlapCount > 0;
         }
@@ -154,46 +206,59 @@ namespace WhalePark18.Manager
         }
 
         /// <summary>
-        /// 적을 소환하는 메소드
+        /// Enemy를 소환하는 메소드
         /// </summary>
-        /// <param name="enemySpawnPoint">적 생성시 미리 알려주는 타일</param>
+        /// <param name="spawnPoint">적 생성시 미리 알려주는 타일</param>
+        /// <param name="spawnEnemyClass">소환할 적의 등급</param>
         /// <returns></returns>
-        private IEnumerator SpawnEnemy(EnemySpawnPoint enemySpawnPoint)
+        private IEnumerator SpawnEnemy(EnemySpawnPoint spawnPoint, EnemyClass spawnEnemyClass)
         {
-            /// 적 소환 대기시간 만큼 대기
-            yield return new WaitForSeconds(enemySpawnDelayTime);
+            /// 1. 적 소환 대기시간 만큼 대기
+            WaitForSeconds waitSpawnEnemyDelayTime = new WaitForSeconds(enemySpawnInfos[(int)spawnEnemyClass].spawnDelayTime);
+            yield return waitSpawnEnemyDelayTime;
 
-            /// enemySpawnPoint 위치에 적 소환
-            Vector3 spawnPosition = enemySpawnPoint.transform.position;
-            //spawnPosition.y = 0;
-            WhalePark18.Debug.Log(DebugCategory.Debug, "Spawn Enemy Position",
-                "Enemy{0} SpawnPosition: {1}", currentEnemyCount, spawnPosition);
+            /// 2. spawnPoint 위치에 적 소환
+            Vector3 spawnPosition = spawnPoint.transform.position;
+            EnemyBase enemy = enemyObjectPools[(int)spawnEnemyClass].GetObject(spawnPosition);
 
-            EnemyFSM enemyFSM = enemyObjectPool.GetObject(spawnPosition);
-            enemyFSM.Setup(target);
-
-            /// enemySpawnTile 오브젝트풀에 반환
-            enemySpawnTileObjectPool.ReturnObject(enemySpawnPoint);
+            /// 3. enemySpawnTile 오브젝트풀에 반환
+            enemySpawnTileObjectPools[(int)spawnEnemyClass].ReturnObject(spawnPoint);
         }
 
         /// <summary>
         /// 적 반환 인터페이스
         /// </summary>
-        /// <param name="returnEnemyFSM"></param>
-        public void ReturnEnemy(EnemyFSM returnEnemyFSM)
+        /// <param name="returnEnemy"></param>
+        public void ReturnEnemy(EnemyBase returnEnemy)
         {
-            /// 오브젝트를 반환하고 현재 적 수와 처치수를 갱신한다.
-            enemyObjectPool.ReturnObject(returnEnemyFSM);
-            currentEnemyCount--;
-            killCount++;
+            /// 1. 적의 등급에 맞게 반환하고 처치 수 정보를 갱신한다.
+            enemyObjectPools[(int)returnEnemy.Class].ReturnObject(returnEnemy);
 
-            if (KillCount == GameManager.Instance.KillGoals)
+            switch(returnEnemy.Class)
+            {
+                case EnemyClass.Normal:
+                    killCountInfo.normal++;
+                    break;
+
+                case EnemyClass.Elite:
+                    killCountInfo.elite++;
+                    break;
+
+                case EnemyClass.Boss:
+                    killCountInfo.boss++;
+                    break;
+            }
+
+            /// 2. Normal 등급 처치 수 >= 목표 처치수 이라면 게임을 종료한다.
+            if (killCountInfo.normal >= GameManager.Instance.GoalNumberOfKill)
                 GameManager.Instance.GameOver();
 
-            /// killCount가 10의 배수일 때마다
-            /// 한번에 소환되는 적의 수를 증가시킨다.
-            if (killCount % 10 == 0)
-                numberOfEnemiesSpawnedAtOnce++;
+            /// 3. killCount가 10의 배수일 때마다
+            if (killCountInfo.normal % 10 == 0)
+            {
+                enemySpawnInfos[(int)EnemyClass.Normal].numberOfSpawnAtOnce++;
+                SpawnEnemyEvent(EnemyClass.Elite);
+            }
         }
     }
 }
