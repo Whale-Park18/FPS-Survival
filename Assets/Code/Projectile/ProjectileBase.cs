256ad32b90b5c21989d3e110f02545a75258e8c9
Whale-Park18 <96812003+Whale-Park18@users.noreply.github.com>
2023-04-12 16:08:09 +0900

    Update ProjectileBase.cs * 유니티 속성 추가     * RequireComponent         * 캡슐 콜라이더 * Projectile 관리용 멤버 변수 추가     * #id: int     * -baseName: string * Projectile 멤버 변수 추가로 인한 프로퍼티 추가     * +ID: int * 초기화(Initialize) 메소드 추가     * +Setup(void): void * +Move(void): void 메소드 변경     * old: +Move(void): void     * new: +Fire(Vector3): void * # abstract Stop(void): void 추가     * 투사체가 작동을 멈출 때, 필요한 작업을 처리하는 메소드 * 주석 추가 및 수정

--- Assets/Code/Projectile/ProjectileBase.cs
+++ Assets/Code/Projectile/ProjectileBase.cs
@@ -1,12 +1,17 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using WhalePark18.Character.Enemy;
 using WhalePark18.Character.Player;
 
 namespace WhalePark18.Projectile
 {
-    public class ProjectileBase : MonoBehaviour
+    [RequireComponent(typeof(CapsuleCollider))]
+    public abstract class ProjectileBase : MonoBehaviour
     {
+        /****************************************
+         * ProjectieBase
+         ****************************************/
         [SerializeField]
         protected float projectileSpeed = 5f;
         [SerializeField]
@@ -14,45 +19,57 @@ namespace WhalePark18.Projectile
         [SerializeField]
         protected int damage = 10;
 
-        public virtual void Move()
+        /****************************************
+         * ProjectileBase 관리용
+         ****************************************/
+        protected int id;
+        [SerializeField]
+        private string baseName;
+
+        /****************************************
+         * 프로퍼티
+         ****************************************/
+        public int ID => id;
+
+        public virtual void Setup(int id)
         {
-            StartCoroutine("OnMove");
+            this.id = id;
+            name = $"{baseName}_{id}";
         }
 
-        protected virtual IEnumerator OnMove()
+        public virtual void Fire(Vector3 targetPosition)
         {
-            float moveDistance = 0f;
-            while(true)
-            {
-                if (moveDistance >= projectileDistance)
-                {
-                    Destroy(gameObject);
-                    yield break;
-                }
+            StartCoroutine(OnMove(targetPosition));
+        }
 
+        /// <summary>
+        /// targetPosition을 향해 projectileSpeed 속도로 
+        /// 이동하는 메소드
+        /// </summary>
+        /// <param name="targetPosition">목표 위치</param>
+        /// <returns>코루틴</returns>
+        /// <remarks>
+        /// 기본적으로 직선으로 움직이지만 투사체에 따라 오버로딩을 활용해
+        /// 목표물을 추적하는 코드로 작성한다.
+        /// </remarks>
+        protected virtual IEnumerator OnMove(Vector3 targetPosition)
+        {
+            Vector3 start = transform.position;
+            transform.LookAt(targetPosition);
+
+            for(float moveDistance = 0; moveDistance < projectileDistance; moveDistance = Vector3.Distance(transform.position, targetPosition))
+            {
                 transform.position += transform.forward * projectileSpeed * Time.deltaTime;
-                moveDistance += projectileSpeed * Time.deltaTime;
 
                 yield return null;
             }
+
+            Stop();
         }
 
-        private void OnTriggerEnter(Collider other)
-        {
-            if (other.CompareTag("ImpactEnemy"))
-            {
-                other.GetComponent<EnemyFSM>().TakeDamage(damage);
-            }
-            else if(other.CompareTag("Player"))
-            {
-                other.GetComponent<PlayerController>().TakeDamage(damage);
-            }
-            else if(other.CompareTag("Intercation"))
-            {
-
-            }
-            
-            Destroy(gameObject);
-        }
+        /// <summary>
+        /// 투사체가 작동을 멈췄을 때, 작동
+        /// </summary>
+        protected abstract void Stop();
     }
 }
