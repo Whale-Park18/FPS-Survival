09a9dce0fdaaf3a834ac6d55fe15cbc2427fd22e
Whale-Park18 <96812003+Whale-Park18@users.noreply.github.com>
2023-04-12 18:49:42 +0900

    Update LOgManager.cs * 열거형 LogCatrgory     * 이름 변경         * old: LogCatrgory         * new: LogType     * 속성 추가         * Debug * 메소드     * -Log(void): void 이름 변경         * old: Log         * new: WriteLogAtFile     * add +ConsoleLog(string, string): static void     * add +ConsoleLog(LogType, string, string): static void     * add +ConsoleDebugLog(string, string): static void     * add +ConsoleErrorLog(string, string): static void     * add +ConsoleWarningLog(string, string): static void * 주석 추가 및 변경

--- Assets/Code/Manager/LogManager.cs
+++ Assets/Code/Manager/LogManager.cs
@@ -5,8 +5,9 @@ using UnityEngine;
 
 namespace WhalePark18.Manager
 {
-    public enum LogCatrgory
+    public enum LogType
     {
+        Debug,
         Error,
         Warning,
         Item,
@@ -15,7 +16,7 @@ namespace WhalePark18.Manager
 
     public struct LogInfo
     {
-        LogCatrgory catrgory;
+        LogType type;
         string fileName;
         Queue<string> context;
     }
@@ -32,6 +33,10 @@ namespace WhalePark18.Manager
         private Queue<LogInfo> tmpQueue = new Queue<LogInfo>();
         private object lockObject;
 
+        /// <summary>
+        /// 로그 파일에 남길 로그 데이터를 큐에 넣는 메소드
+        /// </summary>
+        /// <param name="logInfo"></param>
         public void Enqueue(LogInfo logInfo)
         {
             lock(lockObject)
@@ -40,14 +45,20 @@ namespace WhalePark18.Manager
             }
         }
 
+        /// <summary>
+        /// 저장된 로그 큐를 로그 파일에 작성하는 메소드
+        /// </summary>
         private void Run()
         {
-            Thread logThread = new Thread(Log);
+            Thread logThread = new Thread(WriteLogAtFile);
             logThread.Start();
             logThread.Join();
         }
 
-        private void Log()
+        /// <summary>
+        /// 로그를 파일에 작성하는 메소드
+        /// </summary>
+        private void WriteLogAtFile()
         {
             lock(lockObject)
             {
@@ -56,6 +67,70 @@ namespace WhalePark18.Manager
                     /// TODO: 카테고리에 따라 
                 }
             }
+        }
+
+        /// <summary>
+        /// 콜솔에 로그를 출력하는 메소드
+        /// </summary>
+        /// <param name="title">로그 제목</param>
+        /// <param name="msg">로그 상세 메시지</param>
+        public static void ConsoleLog(string title, string msg)
+        {
+            ConsoleDebugLog(title, msg);
+        }
+
+        /// <summary>
+        /// 콜솔에 로그를 출력하는 메소드
+        /// </summary>
+        /// <param name="title">로그 제목</param>
+        /// <param name="msg">로그 상세 메시지</param>
+        /// <param name="msg"></param>
+        public static void ConsoleLog(LogType catrgory, string title, string msg)
+        {
+            switch(catrgory)
+            {
+                case LogType.Debug:
+                    ConsoleDebugLog(title, msg);
+                    break;
+
+                case LogType.Error:
+                    ConsoleErrorLog(title, msg);
+                    break;
+
+                case LogType.Warning:
+                    ConsoleWarningLog(title, msg);
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// 콘솔에 디버그용 로그를 출력하는 메소드
+        /// </summary>
+        /// <param name="title">로그 제목</param>
+        /// <param name="msg">로그 상세 메시지</param>
+        public static void ConsoleDebugLog(string title, string msg)
+        {
+            UnityEngine.Debug.Log($"<color=green><b>[{title}]</b></color> {msg}");
+        }
+
+        /// <summary>
+        /// 콘솔에 에러용 로그를 출력하는 메소드
+        /// </summary>
+        /// <param name="title">로그 제목</param>
+        /// <param name="msg">로그 상세 메시지</param>
+        public static void ConsoleErrorLog(string title, string msg)
+        {
+            UnityEngine.Debug.Log($"<color=yellow><b>[{title}]</b></color> {msg}");
+        }
+
+        /// <summary>
+        /// 콘솔에 위험용 로그를 출력하는 메소드
+        /// </summary>
+        /// <param name="title">로그 제목</param>
+        /// <param name="msg">로그 상세 메시지</param>
+        public static void ConsoleWarningLog(string title, string msg)
+        {
+            UnityEngine.Debug.Log($"<color=red><b>[{title}]</b></color> {msg}");
         }
     }
 }
