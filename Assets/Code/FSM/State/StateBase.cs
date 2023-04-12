006739afdf42fc8521f9aa453395adf50b983634
Whale-Park18 <96812003+Whale-Park18@users.noreply.github.com>
2023-04-12 23:03:01 +0900

    Add StateBase.cs * 상태를 캡슐화한 인터페이스 클래스

---
+++ Assets/Code/FSM/State/StateBase.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+namespace WhalePark18.FSM.State
+{
+    public abstract class StateBase<T> : MonoBehaviour where T : MonoBehaviour
+    {
+        protected Coroutine coroutine;
+
+        /// <summary>
+        /// 상태에 진입할 때, 처음 호출되는 메소드
+        /// </summary>
+        /// <param name="owner">상태를 가진 Enemy</param>
+        public abstract void Enter(T owner);
+
+        /// <summary>
+        /// 상태를 실행할 때, 호출되는 메소드
+        /// </summary>
+        /// <param name="owner">상태를 가진 Enemy</param>
+        public abstract void Execute(T owner);
+
+        /// <summary>
+        /// 상태를 나갈 때, 호출되는 메소드
+        /// </summary>
+        /// <param name="owner">상태를 가진 Enemy</param>
+        public abstract void Exit(T owner);
+    }
+}
