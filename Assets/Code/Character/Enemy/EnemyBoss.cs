0daacba21cf785344c9240314af5962d3f4c7c41
Whale-Park18 <96812003+Whale-Park18@users.noreply.github.com>
2023-05-19 05:37:27 +0900

    Add EnemyBoss.cs

---
+++ Assets/Code/Character/Enemy/EnemyBoss.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace WhalePark18.Character.Enemy
+{
+    public class EnemyBoss : EnemyBase
+    {
+        public override void CalculateDistanceToTargetAndSelectState()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override void Run()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override void OnStateInitialized()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override void OnVariableInitialized()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
