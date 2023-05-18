f8d893fe5af8aa7f9736473bb10c19195c33b419
Whale-Park18 <96812003+Whale-Park18@users.noreply.github.com>
2023-05-19 05:57:26 +0900

    Update EnemyNavMeshAgentController.cs
    * 정의
        * EnemyNavMeshAgentStates 열거형 정의
    * 프로퍼티
        * NavMeshAgentEnabled: bool 추가
    * 메소드
        * MoveMode(void) 삭제
        * StopMode(BreakMode) 삭제
        * ChangeState(EnemyNavMeshAgentStates, BreakMode) 추가
        * IsArrived(void) 추가

--- Assets/Code/Character/Enemy/EnemyNavMeshAgentController.cs
+++ Assets/Code/Character/Enemy/EnemyNavMeshAgentController.cs
@@ -3,15 +3,21 @@ using System.Collections.Generic;
 using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
 using UnityEngine.AI;
+using static UnityEngine.UI.GridLayoutGroup;
+using WhalePark18.Manager;
 
 namespace WhalePark18.Character.Enemy
 {
+    public enum EnemyNavMeshAgentStates { Move, Stop }
     public enum BreakMode { SmoothStop, SuddenStop }
 
     public class EnemyNavMeshAgentController : MonoBehaviour
     {
         private NavMeshAgent navMeshAgent;
 
+        /****************************************
+         * 프로퍼티
+         ****************************************/
         public float speed
         {
             get => navMeshAgent.speed;
@@ -19,10 +25,17 @@ namespace WhalePark18.Character.Enemy
         }
 
         public Vector3 destination => navMeshAgent.destination;
+
         public float stoppingDistance
         {
             set => navMeshAgent.stoppingDistance = value;
             get => navMeshAgent.stoppingDistance;
+        }
+
+        public bool NavMeshAgentEnabled
+        {
+            set => navMeshAgent.enabled = value;
+            get => navMeshAgent.enabled;
         }
 
         private void Awake()
@@ -48,26 +61,43 @@ namespace WhalePark18.Character.Enemy
         }
 
         /// <summary>
-        /// 이동 모드로 바꾸는 메소드
+        /// NavMeshAgent의 상태를 변경하는 메소드
         /// </summary>
-        public void MoveMode()
+        /// <param name="newState">변경할 상태</param>
+        /// <param name="breakMode">"Stop" 상태일 때, 정지 방법</param>
+        public void ChangeState(EnemyNavMeshAgentStates newState, BreakMode breakMode = BreakMode.SuddenStop)
         {
-            navMeshAgent.isStopped = false;
-            //navMeshAgent.updatePosition = true;
-            //navMeshAgent.updateRotation = true;
+            if (newState.Equals(EnemyNavMeshAgentStates.Move))
+            {
+                navMeshAgent.isStopped = false;
+            }
+            else if(newState.Equals(EnemyNavMeshAgentStates.Stop))
+            {
+                navMeshAgent.isStopped = true;
+
+                if(breakMode.Equals(BreakMode.SuddenStop)) navMeshAgent.velocity = Vector3.zero;
+            }
         }
 
         /// <summary>
-        /// 정지 모드로 바꾸는 메소드
+        /// 에이전트가 도착 여부를 반환하는 메소드
         /// </summary>
-        /// <param name="breakMode">정지 모드</param>
-        public void StopMode(BreakMode breakMode)
+        /// <returns>도착 여부</returns>
+        public bool IsArrived()
         {
-            navMeshAgent.isStopped = true;
-            //navMeshAgent.updatePosition = false;
-            //navMeshAgent.updateRotation = false;
+            /// 에이전트가 이동 명령을 받은 후, 경로를 계산 중이라면 pathPending이 활성화 되며,
+            /// 목적지까지 이동 중이라면 hasPath이 활성화 된다. 목적지에 도착하면 비활성화 된다.
+            /// 이를 이용해 경로 계산 중이거나 이동 중임을 확인할 수 있다.
+            /// hasPath과 pathPending이 모두 비활성화 됐을 때, 남은 거리가 정지 거리 이하라면 도착했음을 의미한다.
+            if (navMeshAgent.hasPath == false && navMeshAgent.pathPending == false)
+            {
+                if (navMeshAgent.remainingDistance <= navMeshAgent.stoppingDistance)
+                {
+                    return true;
+                }
+            }
 
-            if(breakMode == BreakMode.SuddenStop) navMeshAgent.velocity = Vector3.zero;
+            return false;
         }
     }
 }
