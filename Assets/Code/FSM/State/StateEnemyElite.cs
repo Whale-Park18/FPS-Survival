8c52576359f0bd62877e20afa4cef170cb53cab0
Whale-Park18 <96812003+Whale-Park18@users.noreply.github.com>
2023-06-11 23:02:29 +0900

    Update StateEnemyElite.cs
    * 공통
        * 멤버 변수 coroutine의 이름 변경에 따른 수정
        * 애니메이터 수정에 따른 호출 함수 수정
            * old: SetBool
            * new: SetFloat
    * Idle
        * 메소드
            * Execute(EnemyElite): void
                * coroutineHandle을 초기화하지 않아 상태 종료 후에도 코루틴이 종료되지 않는 현상 수정.
            * OnIdle(EnemyElite): IEnumerator
                * EnemyElite 개발 중 주석화된 코드 복구
    * Wander
        * 메소드
            * Enter(EnemyElite): void
                * EnemyElite는 근접 공격 몬스터이기 때문에 에이전트의 '정지 거리'를 수정할 필요가 없음.
                * 어떤 상태에서 진입했는지 알 수 없기 때문에 에이전트의 상태를 이동 상태로 전환
            * OnWAnder(EnemyElite): IEnumerator
                * 디버그용 코드 추가
    * Pursuit
        * 메소드
            * Enter(EnemyElite): void
                * 어떤 상태에서 진입했는지 알 수 없기 때문에 에이전트의 상태를 이동 상태로 전환
            * OnPursuit(EnemyElite): IEnumerator
                * 디버그용 코드 추가
    * Attack
        * 멤버 변수
            * EnemyElite의 멤버 변수 구성의 변경에 따른 멤버 변수 간략화
                * old: EnemyElite에서 private 접근 제한자로 갖던 공격 변수들을 Setup(float, float, float): void을 호출하여 초기화
                * new: public 접근 제한자로 변경돼, 쉽게 접근 가능해짐
        * 메소드
            * Setup(float, float, float): void 삭제
            * Enter(EnemyElite): void
                * 멤버 변수 간략화에 따른 매개 변수 수정
                * 공격 중, 상태가 변경되지 않도록 '상태 변경 가능' 플래그 수정
                * 에이전트의 이동 속도를 '돌진 속도'로 수정
                * 근접 공격의 공격력을 '돌진 공격력'으로 수정
            * Exit(EnemyElite): void
                * 근접 공격의 공격력 '일반 공격력'으로 복구
            * OnAttack(EnemyElite): IEnumerator
                * 공격 완료 후, '상태 변경 가능 플래그' 수정
                * 공격 완료 후, 거리에 따라 상태를 변경하도록 수정
            * OnMoveToDestiantion(EnemyElite): IEnumerator
                * 디버그용 코드 추가

--- Assets/Code/FSM/State/StateEnemyElite.cs
+++ Assets/Code/FSM/State/StateEnemyElite.cs
@@ -16,7 +16,7 @@ namespace WhalePark18.FSM.State.StateEnemyElite
             LogManager.ConsoleDebugLog($"[{gameObject.transform.parent.name}] Idle", "Enter");
 
             /// 애니메이션 설정 후, Idle 상태를 실행한다.
-            owner.AnimatorController.SetBool(EnemyNormalAnimParam.isMovement.ToString(), false);
+            owner.AnimatorController.SetFloat(EnemyNormalAnimParam.Speed.ToString(), 0);
 
             Execute(owner);
         }
@@ -25,8 +25,8 @@ namespace WhalePark18.FSM.State.StateEnemyElite
         {
             // #DEBUG
             LogManager.ConsoleDebugLog($"[{gameObject.transform.parent.name}] Idle", "Execute");
-            
-            OnIdle(owner);
+
+            coroutineHandle = StartCoroutine(OnIdle(owner));
         }
 
         public override void Exit(EnemyElite owner)
@@ -35,9 +35,9 @@ namespace WhalePark18.FSM.State.StateEnemyElite
             LogManager.ConsoleDebugLog($"[{gameObject.transform.parent.name}] Idle", "Exit");
 
             /// Idle 코루틴 정지
-            if (coroutine != null)
+            if (coroutineHandle != null)
                 StopAllCoroutines();
-            coroutine = null;
+            coroutineHandle = null;
         }
 
         /// <summary>
@@ -45,18 +45,18 @@ namespace WhalePark18.FSM.State.StateEnemyElite
         /// </summary>
         /// <param name="owner">상태를 가진 Enemy</param>
         /// <returns></returns>
-        private void OnIdle(EnemyElite owner)
+        private IEnumerator OnIdle(EnemyElite owner)
         {
             /// 0 ~ MaxIdleTime 시간 동안 기다린 후, Wander 상태로 변경한다.
             float idleTime = Random.Range(1, owner.MaxIdleTime);
             StartCoroutine(AutoChangeFromIdleToWander(idleTime, owner));
 
-            //while (true)
-            //{
-            //    owner.CalculateDistanceToTargetAndSelectState();
-            //
-            //    yield return null;
-            //}
+            while (true)
+            {
+                owner.CalculateDistanceToTargetAndSelectState();
+            
+                yield return null;
+            }
         }
 
         /// <summary>
@@ -81,9 +81,9 @@ namespace WhalePark18.FSM.State.StateEnemyElite
             LogManager.ConsoleDebugLog($"[{gameObject.transform.parent.name}] Wander", "Enter");
 
             /// 애니메이션 설정 후, Wander 상태를 실행한다.
-            owner.AnimatorController.SetBool(EnemyNormalAnimParam.isMovement.ToString(), true);
+            owner.AnimatorController.SetFloat(EnemyNormalAnimParam.Speed.ToString(), owner.Status.WalkSpeed);
             owner.NavMeshAgentController.speed = owner.Status.WalkSpeed;
-            owner.NavMeshAgentController.stoppingDistance = 0;
+            owner.NavMeshAgentController.ChangeState(EnemyNavMeshAgentStates.Move);
 
             Execute(owner);
         }
@@ -93,7 +93,7 @@ namespace WhalePark18.FSM.State.StateEnemyElite
             // #DEBUG
             LogManager.ConsoleDebugLog($"[{gameObject.transform.parent.name}] Wander", "Execute");
 
-            coroutine = StartCoroutine(OnWander(owner));
+            coroutineHandle = StartCoroutine(OnWander(owner));
         }
 
         public override void Exit(EnemyElite owner)
@@ -102,12 +102,12 @@ namespace WhalePark18.FSM.State.StateEnemyElite
             LogManager.ConsoleDebugLog($"[{gameObject.transform.parent.name}] Wander", "Exit");
 
             /// Wander 코루틴 정지
-            if (coroutine != null)
+            if (coroutineHandle != null)
                 StopAllCoroutines();
-            coroutine = null;
+            coroutineHandle = null;
 
             // #DEBUG
-            LogManager.ConsoleDebugLog($"[{gameObject.transform.parent.name}] Wander", $"coroutine is null: {coroutine == null}");
+            LogManager.ConsoleDebugLog($"[{gameObject.transform.parent.name}] Wander", $"coroutine is null: {coroutineHandle == null}");
 
             /// 경로 초기화
             owner.NavMeshAgentController.ResetPath();
@@ -121,7 +121,10 @@ namespace WhalePark18.FSM.State.StateEnemyElite
         private IEnumerator OnWander(EnemyElite owner)
         {
             Vector3 wanderPosition = owner.CalculateWanderPosition();
-            owner.NavMeshAgentController.SetDestination(wanderPosition);
+            if (GameManager.Instance.DebugMode && GameManager.Instance.DevelopingEnemy)
+                owner.NavMeshAgentController.SetDestination(wanderPosition, owner.NavDestinationShowObject.transform);
+            else
+                owner.NavMeshAgentController.SetDestination(wanderPosition);
 
             // #DEBUG
             LogManager.ConsoleDebugLog($"[{gameObject.transform.parent.name}] Wander", $"wanderPosition: {wanderPosition}");
@@ -162,8 +165,9 @@ namespace WhalePark18.FSM.State.StateEnemyElite
             LogManager.ConsoleDebugLog($"[{gameObject.transform.parent.name}] Pursuit", "Enter");
 
             /// 애니메이션 파라미터, 이동 속도를 설정한 후, Pursuit 상태 실행
-            owner.AnimatorController.SetBool(EnemyNormalAnimParam.isMovement.ToString(), true);
+            owner.AnimatorController.SetFloat(EnemyNormalAnimParam.Speed.ToString(), owner.Status.RunSpeed);
             owner.NavMeshAgentController.speed = owner.Status.RunSpeed;
+            owner.NavMeshAgentController.ChangeState(EnemyNavMeshAgentStates.Move);
 
             Execute(owner);
         }
@@ -173,7 +177,7 @@ namespace WhalePark18.FSM.State.StateEnemyElite
             // #DEBUG
             LogManager.ConsoleDebugLog($"[{gameObject.transform.parent.name}] Pursuit", "Execute");
 
-            coroutine = StartCoroutine(OnPursuit(owner));
+            coroutineHandle = StartCoroutine(OnPursuit(owner));
         }
 
         public override void Exit(EnemyElite owner)
@@ -181,9 +185,9 @@ namespace WhalePark18.FSM.State.StateEnemyElite
             LogManager.ConsoleDebugLog($"[{gameObject.transform.parent.name}] Pursuit", "Exit");
 
             /// Pursuit 코루틴 정지
-            if (coroutine != null)
+            if (coroutineHandle != null)
                 StopAllCoroutines();
-            coroutine = null;
+            coroutineHandle = null;
 
             /// 경로 초기화
             owner.NavMeshAgentController.ResetPath();
@@ -200,7 +204,10 @@ namespace WhalePark18.FSM.State.StateEnemyElite
             while (true)
             {
                 /// 개체의 속도를 RunSpeed로 바꾸고 타겟의 위치로 경로를 갱신한다.
-                owner.NavMeshAgentController.SetDestination(owner.Target.position);
+                if (GameManager.Instance.DebugMode && GameManager.Instance.DevelopingEnemy)
+                    owner.NavMeshAgentController.SetDestination(owner.Target.position, owner.NavDestinationShowObject.transform);
+                else
+                    owner.NavMeshAgentController.SetDestination(owner.Target.position);
                 owner.LookRotationToTarget();
 
                 owner.CalculateDistanceToTargetAndSelectState();
@@ -212,83 +219,68 @@ namespace WhalePark18.FSM.State.StateEnemyElite
 
     public class Attack : StateBase<EnemyElite>
     {
-        public float stopTimeBeforeAttack;
-        public float stopTimeAfterAttack;
-        public Vector3 dashDestination;
-        public float dashSpeed;
-
-        private WaitForSeconds waitStopBeforeAttack;
-        private WaitForSeconds waitStopAfterAttack;
-
-        public void Setup(float stopTimeBeforeAttack, float stopTimeAfterAttack, float attackMoveSpeed)
-        {
-            this.stopTimeBeforeAttack = stopTimeBeforeAttack;
-            this.stopTimeAfterAttack = stopTimeAfterAttack;
-            this.dashSpeed = attackMoveSpeed;
-        }
+        private WaitForSeconds _waitStopBeforeAttack;
+        private WaitForSeconds _waitStopAfterAttack;
 
         public override void Enter(EnemyElite owner)
         {
-            LogManager.ConsoleDebugLog("Attack<Elite>", "Enter");
+            LogManager.ConsoleDebugLog($"[{gameObject.transform.parent.name}] Attack", "Enter");
 
-            waitStopBeforeAttack = new WaitForSeconds(stopTimeBeforeAttack);
-            waitStopAfterAttack = new WaitForSeconds(stopTimeAfterAttack);
+            _waitStopBeforeAttack = new WaitForSeconds(owner.WaitTimeBeforeAttack);
+            _waitStopAfterAttack = new WaitForSeconds(owner.WaitTimeAfterAttack);
 
-            owner.AnimatorController.GetAnimationClip(EnemyEliteAnimName.Attack.ToString());
+            owner.SetCanChangeState(false);
+            owner.NavMeshAgentController.speed = owner.RushSpeed;
+            owner.SetDamage(owner.RushDamage);
 
             Execute(owner);
         }
 
         public override void Execute(EnemyElite owner)
         {
-            LogManager.ConsoleDebugLog("Attack<Elite>", "Execute");
+            LogManager.ConsoleDebugLog($"[{gameObject.transform.parent.name}] Attack", "Execute");
 
-            StartCoroutine(OnAttack(owner));
+            coroutineHandle = StartCoroutine(OnAttack(owner));
         }
 
         public override void Exit(EnemyElite owner)
         {
-            LogManager.ConsoleDebugLog("Attack<Elite>", "Exit");
+            LogManager.ConsoleDebugLog($"[{gameObject.transform.parent.name}] Attack", "Exit");
 
-            owner.NavMeshAgentController.ChangeState(EnemyNavMeshAgentStates.Move);
+            if (coroutineHandle != null)
+                StopAllCoroutines();
+            coroutineHandle = null;
+
+            owner.SetDamage(owner.NormalDamage);
         }
 
         private IEnumerator OnAttack(EnemyElite owner)
         {
-            /// NavMeshAgent 상태를 "Stop"으로 변경한 후, stopTimeBeforeAttack초 만큼 대기
-            LogManager.ConsoleDebugLog("Attack<Elite>", "공격 전 대기");
+            LogManager.ConsoleDebugLog($"{name} Attack", "Part. 1 공격 전 대기");
             {
                 owner.NavMeshAgentController.ChangeState(EnemyNavMeshAgentStates.Stop);
-                owner.AnimatorController.SetBool(EnemyEliteAnimParam.isMovement.ToString(), false);
-                yield return waitStopBeforeAttack;
+                owner.AnimatorController.SetFloat(EnemyEliteAnimParam.Speed.ToString(), 0);
+                yield return _waitStopBeforeAttack;
             }
-
-
-            /// "Attack" 애니메이션 활성화
-            /// NavMeshAgent 상태와 속도를 변경한 후, 목적지를 돌진 목적지로 설정한다.
-            LogManager.ConsoleDebugLog("Attack<Elite>", "공격 중");
+            
+            LogManager.ConsoleDebugLog($"{name} Attack", "Part. 2 공격");
             {
                 owner.NavMeshAgentController.ChangeState(EnemyNavMeshAgentStates.Move);
-                owner.NavMeshAgentController.speed = owner.DashSpeed;
-                owner.NavMeshAgentController.SetDestination(dashDestination);
                 owner.AnimatorController.SetBool(EnemyEliteAnimParam.isAttack.ToString(), true);
-
                 yield return StartCoroutine(OnMoveToDestination(owner));
-            }
-
-            /// stopTimeAfterAttack초 만큼 대기 후, "Wander" 상태로 변경
-            LogManager.ConsoleDebugLog("Attack<Elite>", "공격 후 대기");
-            {
                 owner.NavMeshAgentController.ChangeState(EnemyNavMeshAgentStates.Stop, BreakMode.SmoothStop);
-                owner.NavMeshAgentController.speed = owner.Status.WalkSpeed;
-                owner.AnimatorController.SetBool(EnemyEliteAnimParam.isAttack.ToString(), false);
-
-                yield return waitStopAfterAttack;
             }
-
-            LogManager.ConsoleDebugLog("Attack<Elite>", "상태 전환");
+            
+            LogManager.ConsoleDebugLog($"{name} Attack", "Part. 3 공격 후 대기");
             {
-                owner.ChangeState(EnemyEliteStates.Wander);
+                owner.AnimatorController.SetBool(EnemyEliteAnimParam.isAttack.ToString(), false);
+                yield return _waitStopAfterAttack;
+            }
+            
+            LogManager.ConsoleDebugLog($"{name} Attack", "Part. 4 상태 변경");
+            {
+                owner.SetCanChangeState(true);
+                owner.CalculateDistanceToTargetAndSelectState();
             }
         }
 
@@ -299,17 +291,15 @@ namespace WhalePark18.FSM.State.StateEnemyElite
         /// <returns></returns>
         private IEnumerator OnMoveToDestination(EnemyElite owner)
         {
-            owner.NavMeshAgentController.SetDestination(dashDestination);
+            if (GameManager.Instance.DebugMode && GameManager.Instance.DevelopingEnemy)
+                owner.NavMeshAgentController.SetDestination(owner.Target.position, owner.NavDestinationShowObject.transform);
+            else
+                owner.NavMeshAgentController.SetDestination(owner.Target.position);
 
             var waitUntilOneArrive = new WaitUntil(
                 () => { return owner.NavMeshAgentController.IsArrived(); } 
             );
             yield return waitUntilOneArrive;
-        }
-
-        private void ChangeSpeed(EnemyElite owner, float speed)
-        {
-            owner.NavMeshAgentController.speed = speed;
         }
     }
 }
