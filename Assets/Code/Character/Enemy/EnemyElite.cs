3c26487e1fd98ef690efda3a3fc53521f29142be
Whale-Park18 <96812003+Whale-Park18@users.noreply.github.com>
2023-06-11 22:22:04 +0900

    Update EnemyElite.cs
    * 정의
        * EnemyEliteAnimParam
            * isMovement 이름 변경
                * old: isMovement
                * new: Speed
    * 멤버 변수 선언
        * 멤버 변수 선언 방식 변경
            * old: private + [SerializeField]
            * new: [SerializeField] 멤버 변수 -> public
        * 앞으로 이런 방식으로 개편할 것
    * 메소드
        * Start(void): void
            * 디버그용 코드 추가
        * Update(void): void
            * 근접 공격과 플레이어 탐색 메소드 호출
        * OnVariableInitialized(void): void
            * 멤버 변수 선언 방식 변경으로 인한 초기화 코드 추가
        * ScanPlayer(void): void 추가
            * 전방 MaxAttackDistance 범위에 플레이어가 있는지 확인
            * 플레이어가 확인되면 공격 상태로 변경
        * MeleeAttack(void): void 추가
            * Collider의 트리거를 이용한 방식에서 Physics.OverlapSphere을 이용한 방식으로변경
            * 일반 공격과 돌진 공격에 따라 공격력이 수정됨.
                * 단, 공격력 수정은 Attack 상태에서 처리함.
        * CalculateDistanceToTargetAndSelectState(void): void
            * Attack 상태 중, 상태가 변경되지 않도록 '상태 변경 가능' 플래그를 이용해 메소드 종료
        * ChangeState(EnemyEliteStates): void
            * 디버그용 코드 추가
        * SetCanChangeState(bool, string): void 추가
        * SetDanage(int): 추가

--- Assets/Code/Character/Enemy/EnemyElite.cs
+++ Assets/Code/Character/Enemy/EnemyElite.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using WhalePark18.Character.Player;
 using WhalePark18.FSM;
 using WhalePark18.FSM.State;
 using WhalePark18.FSM.State.StateEnemyElite;
@@ -10,8 +10,8 @@ using WhalePark18.Manager;
 namespace WhalePark18.Character.Enemy
 {
     public enum EnemyEliteStates { Idle = 0, Wander, Pursuit, Attack, }
-    public enum EnemyEliteAnimParam { isMovement, isAttack, doDie, }
-    public enum EnemyEliteAnimName {  Idle, Walk, Attack, Die }
+    public enum EnemyEliteAnimParam { Speed, isAttack, doDie, }
+    public enum EnemyEliteAnimName { Idle, Walk, Attack, Die }
 
     public class EnemyElite : EnemyBase
     {
@@ -24,58 +24,88 @@ namespace WhalePark18.Character.Enemy
         protected bool canChangeState = true;
 
         [Header("Attack")]
-        [SerializeField]
-        private float attackRate = 1f;                      // 공격 주기
-        private float laskAttackTime = 0;
-        [SerializeField]
-        private float attackDistance = 5f;                  // 공격 범위(이 범위 안에 들어오면 "Attack"상태로 변경)
-        [SerializeField]
-        private float dashSpeed;
-        private Ray forwardRay;
-        private RaycastHit forwardRayHit;
-        [SerializeField]
-        private float stopTimeBeforeAttack;
-        [SerializeField]
-        private float stopTimeAfterAttack;
 
-        [SerializeField] private Transform colliderTransform;
-        [SerializeField] private float colliderRadius = 1f;
-        [SerializeField] private LayerMask playerLayer = 0;
+        [Tooltip("일반 공격력")]
+        public int NormalDamage;
 
-        /****************************************
-         * 프로퍼티
-         ****************************************/
-        public float AttackDistance => attackDistance;
-        public float Radius => colliderRadius;
-        public LayerMask PlayerLayer => playerLayer;
-        public float DashSpeed => dashSpeed;
+        [Tooltip("돌진 공격력")]
+        public int RushDamage;
+
+        [Tooltip("일반 공격 주기(초)")]
+        public float NormalAttackCycle;
+
+        [Tooltip("돌진 공격 주기(초)")]
+        public float RushAttackCycle;
+
+        [Tooltip("최대 공격 거리(m)")]
+        public float MaxAttackDistance;
+
+        [Tooltip("돌진 속도(m/s)")]
+        public float RushSpeed;
+
+        [Tooltip("돌진 전 대기시간(초)")]
+        public float WaitTimeBeforeAttack;
+
+        [Tooltip("돌진 후 대기시간(초)")]
+        public float WaitTimeAfterAttack;
+
+        [HideInInspector]
+        public Vector3 AttackDestination;
+
+        [Space(10)]
+        [Tooltip("공격 콜라이더 오프셋(m)")]
+        public float AttackColliderOffset;
+
+        [Tooltip("공격 콜라이더 반지름(m)")]
+        public float AttackColliderRadius;
+
+        [Tooltip("플레이어 레이어 마스크")]
+        public LayerMask PlayerMask;
+
+        [Tooltip("몬스터 중심")]
+        public Transform Center;
+
+        // Attack
+        private int     _damage;
+        private float   _lastAttackTime;
+        private float   _lastRushTime = 0;
+
+        // Scan
+        private Ray         _scanRay;
+        private RaycastHit  _scanRayHitInfo;
+        private bool        _scanHit;
+
+        [Header("Debug")]
+
+        [Tooltip("행동 정지 플래그")]
+        public bool DontAction;
+
+        [Tooltip("Nav 에이전트 목적지 표시 오브젝트")]
+        public GameObject NavDestinationShowObject;
+
 
         private void Start()
         {
             // #DEBUG
-            //Setup(0);
-            //ChangeState(EnemyEliteStates.Idle);
+            if(GameManager.Instance.DebugMode && GameManager.Instance.DevelopingEnemy)
+            {
+                Setup(0);
+                ChangeState(EnemyEliteStates.Idle);
+            }
         }
 
         private void Update()
         {
-            forwardRay = new Ray(colliderTransform.position, transform.forward);
-            var isHit = Physics.SphereCast(forwardRay, colliderRadius, out forwardRayHit, attackDistance, playerLayer);
-
-            if (isHit && Time.time - laskAttackTime > attackRate && currentState != EnemyEliteStates.Attack)
-            {
-                var dashDestination = forwardRayHit.point;
-                dashDestination.y = 0;
-                LogManager.ConsoleDebugLog("Elite", $"dashDestination: {dashDestination}");
-                ((Attack)states[(int)EnemyEliteStates.Attack]).dashDestination = dashDestination;
-
-                ChangeState(EnemyEliteStates.Attack);
-            }
+            MeleeAttack();
+            ScanPlayer();
         }
 
         protected override void OnVariableInitialized()
         {
             base.OnVariableInitialized();
+
+            _damage = NormalDamage;
+            _lastRushTime -= RushAttackCycle;
         }
 
         protected override void OnStateInitialized()
@@ -92,18 +122,46 @@ namespace WhalePark18.Character.Enemy
             states[(int)EnemyEliteStates.Pursuit] = statesObject.AddComponent<Pursuit>();
             states[(int)EnemyEliteStates.Attack] = statesObject.AddComponent<Attack>();
 
-            ((Attack)states[(int)EnemyEliteStates.Attack]).Setup(
-                stopTimeBeforeAttack, stopTimeAfterAttack, dashSpeed
-            );
-
             /// 3. 상태 머신 초기화
             stateMachine = new StateMachine<EnemyElite>();
             stateMachine.Setup(this);
         }
 
-        public override void Setup(int id)
+        /// <summary>
+        /// 플레이어 스캔 메소드
+        /// </summary>
+        private void ScanPlayer()
         {
-            base.Setup(id);
+            _scanRay = new Ray(Center.position, transform.forward);
+            _scanHit = Physics.SphereCast(_scanRay, AttackColliderRadius, out _scanRayHitInfo, MaxAttackDistance, PlayerMask);
+
+            /// 스캔 성공 && 공격 주기 && 현재 상태가 공격이 아니라면 공격 실행
+            if (_scanHit && Time.time - _lastRushTime > RushAttackCycle && currentState != EnemyEliteStates.Attack)
+            {
+                _lastRushTime = Time.time;
+
+                AttackDestination = _scanRayHitInfo.point;
+                AttackDestination.y = 0;
+                LogManager.ConsoleDebugLog($"{name}", $"AttackDestination: {AttackDestination}");
+
+                ChangeState(EnemyEliteStates.Attack);
+            }
+        }
+
+        /// <summary>
+        /// 근접 공격
+        /// </summary>
+        private void MeleeAttack()
+        {
+            Collider[] colliders = Physics.OverlapSphere(Center.position + transform.forward * AttackColliderOffset, AttackColliderRadius, PlayerMask);
+   
+            if ( colliders.Length > 0 && Time.time - _lastAttackTime > NormalAttackCycle)
+            {
+                _lastAttackTime = Time.time;
+            
+                var player = colliders[0].GetComponent<PlayerController>();
+                player.TakeDamage(_damage);
+            }
         }
 
         public override void Run()
@@ -123,6 +181,11 @@ namespace WhalePark18.Character.Enemy
                 return;
             }
 
+            if (canChangeState == false)
+            {
+                return;
+            }
+
             float distance = Vector3.Distance(target.position, transform.position);
             if (distance <= pursuitDistance)
             {
@@ -135,22 +198,54 @@ namespace WhalePark18.Character.Enemy
             }
         }
 
+        /// <summary>
+        /// 상태 변경 메소드
+        /// </summary>
+        /// <param name="newState"></param>
         public void ChangeState(EnemyEliteStates newState)
         {
+            if (DontAction) return;
+
             currentState = newState;
             stateMachine.ChangeState(states[(int)newState]);
         }
 
+        /// <summary>
+        /// 상태 변경 가능 플래그 설정 메소드
+        /// </summary>
+        /// <param name="canChange"></param>
+        /// <param name="callerName"></param>
+        public void SetCanChangeState(bool canChange, [System.Runtime.CompilerServices.CallerMemberName] string callerName = "")
+        {
+            LogManager.ConsoleDebugLog("SetCanChangeState", $"Caller: {callerName}, Set: {canChange}");
+            this.canChangeState = canChange;
+        }
+
+        public void SetDamage(int damage)
+        {
+            _damage = damage;
+        }
+
         private void OnDrawGizmos()
         {
+            /// 공격 범위
+            {
+                Gizmos.color = Color.red;
+                Gizmos.DrawWireSphere(Center.position + transform.forward * AttackColliderOffset, AttackColliderRadius);
+            }
+
             /// 돌진
             {
                 Gizmos.color = Color.blue;
 
-                Gizmos.DrawRay(colliderTransform.position, transform.forward * forwardRayHit.distance);
-                Gizmos.DrawWireSphere(colliderTransform.position + transform.forward * forwardRayHit.distance, colliderRadius);
+                var gizmoDistance = _scanHit ? _scanRayHitInfo.distance : MaxAttackDistance;
+                Gizmos.DrawRay(Center.position, transform.forward * gizmoDistance);
+                Gizmos.DrawWireSphere(Center.position + transform.forward * gizmoDistance, AttackColliderRadius);
             }
+        }
 
+        private void OnDrawGizmosSelected()
+        {
             /// 추적 범위
             {
                 Gizmos.color = Color.red;
@@ -165,6 +260,8 @@ namespace WhalePark18.Character.Enemy
 
             /// 현재 위치에서 도착지까지의 직선 거리
             {
+                if (navMeshAgentController == null) return;
+
                 Gizmos.color = Color.black;
                 Gizmos.DrawRay(transform.position, navMeshAgentController.destination - transform.position);
             }
