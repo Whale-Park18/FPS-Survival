e6fd2b9260aec2226f06f69a3e9665e99f5dc577
Whale-Park18 <96812003+Whale-Park18@users.noreply.github.com>
2023-06-17 18:17:35 +0900

    Update UerSetting/Mouse, Sond.cs
    * 상속 구조 변경
        * ScriptableObject는 게임 실행될 때마다 설정이 초기호됨.
        * Json으로 정보를 백업하고 게임 실행시 불러오려고 했으나 ScriptableObject는 JsonUtiltiy가 미지원.
        * 위와 같은 이유로 ScriptabltObject 상속X

--- Assets/Code/UserSetting/Mouse.cs
+++ Assets/Code/UserSetting/Mouse.cs
@@ -5,15 +5,21 @@ using UnityEngine;
 
 namespace WhalePark18.UserSetting
 {
-    [CreateAssetMenu(fileName = "Mouse Setting", menuName = "ScriptObject/Mouse Setting")]
-    public class Mouse : ScriptableObject
+    [Serializable]
+    public class Mouse
     {
         [Tooltip("熱霜 團馬紫")]
         [Range(0f, 1f)]
-        public float VirticalSensitivity;
+        public float VerticalSensitivity;
 
         [Tooltip("熱ゎ 團馬紫")]
         [Range(0f, 1f)]
         public float HorizontalSensitivity;
+
+        public Mouse(float verticalSensitivity, float horizontalSensitivity)
+        {
+            VerticalSensitivity = verticalSensitivity;
+            HorizontalSensitivity = horizontalSensitivity;
+        }
     }
 }
--- Assets/Code/UserSetting/Sound.cs
+++ Assets/Code/UserSetting/Sound.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 
 namespace WhalePark18.UserSetting
 {
-    [CreateAssetMenu(fileName = "Sound Setting", menuName = "ScriptObject/Sound Setting")]
-    public class Sound : ScriptableObject
+    [Serializable]
+    public class Sound
     {
         [Tooltip("음소거")]
         public bool Mute;
@@ -26,5 +26,14 @@ namespace WhalePark18.UserSetting
         [Tooltip("노래 음량")]
         [Range(0f, 1f)]
         public float MusicVolume;
+
+        public Sound(bool mute, float masterVolume, float playerVolume, float itemVolume, float musicVolume)
+        {
+            Mute = mute;
+            MasterVolume = masterVolume;
+            PlayerVolume = playerVolume;
+            ItemVolume = itemVolume;
+            MusicVolume = musicVolume;
+        }
     }
 }
